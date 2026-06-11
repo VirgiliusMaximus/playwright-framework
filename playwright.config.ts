94aaf51705cd9802acfc7acf9575a4e8d8577ff3
CorneliusMaximus <virgilus@gmail.com>
2026-06-11 13:15:15 +0300

    final

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,15 +19,15 @@ dotenv.config({
 export default defineConfig({
   testDir: './tests/ui-test',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'never' }],['junit', { outputFile: 'test-results/test-results.xml' }],['allure-playwright', { outputFolder: 'allure-results' }]],
+  reporter: [['html', { open: 'never',outputFolder: 'test-results'}],['junit', { outputFile: 'test-results/test-results.xml' }],['allure-playwright', { outputFolder: 'allure-results' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
